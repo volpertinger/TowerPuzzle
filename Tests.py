920b6d68996830bc7471ccf4f08a1877ffd46216
volpertinger <merzlovnik@mail.ru>
2021-11-12 15:31:52 +0300

    visibility_count rework

--- Tests.py
+++ Tests.py
@@ -20,25 +20,45 @@ class Test_class(unittest.TestCase):
         ss = TowerPuzzle.TowerPuzzle(visibility)
         ss.field = field
 
-        self.assertEqual(ss.count_visibility_horizontal(0), 3)
-        self.assertEqual(ss.count_visibility_horizontal(1), 2)
-        self.assertEqual(ss.count_visibility_horizontal(2), 1)
+        self.assertEqual(ss.count_visibility_left(0), 3)
+        self.assertEqual(ss.count_visibility_left(1), 2)
+        self.assertEqual(ss.count_visibility_left(2), 1)
 
-        self.assertEqual(ss.count_visibility_vertical(0), 3)
-        self.assertEqual(ss.count_visibility_vertical(1), 2)
-        self.assertEqual(ss.count_visibility_vertical(2), 1)
+        self.assertEqual(ss.count_visibility_right(0), 1)
+        self.assertEqual(ss.count_visibility_right(1), 2)
+        self.assertEqual(ss.count_visibility_right(2), 2)
+
+        self.assertEqual(ss.count_visibility_up(0), 3)
+        self.assertEqual(ss.count_visibility_up(1), 2)
+        self.assertEqual(ss.count_visibility_up(2), 1)
+
+        self.assertEqual(ss.count_visibility_down(0), 1)
+        self.assertEqual(ss.count_visibility_down(1), 2)
+        self.assertEqual(ss.count_visibility_down(2), 2)
 
         field = [[3, 2, 5, 4, 1], [4, 5, 3, 1, 2], [5, 4, 1, 2, 3], [2, 1, 4, 3, 5], [1, 3, 2, 5, 4]]
         ss.field = field
 
-        self.assertEqual(ss.count_visibility_horizontal(0), 2)
-        self.assertEqual(ss.count_visibility_horizontal(1), 2)
-        self.assertEqual(ss.count_visibility_horizontal(2), 1)
-        self.assertEqual(ss.count_visibility_horizontal(3), 3)
-        self.assertEqual(ss.count_visibility_horizontal(4), 3)
+        self.assertEqual(ss.count_visibility_left(0), 2)
+        self.assertEqual(ss.count_visibility_left(1), 2)
+        self.assertEqual(ss.count_visibility_left(2), 1)
+        self.assertEqual(ss.count_visibility_left(3), 3)
+        self.assertEqual(ss.count_visibility_left(4), 3)
 
-        self.assertEqual(ss.count_visibility_vertical(0), 3)
-        self.assertEqual(ss.count_visibility_vertical(1), 2)
-        self.assertEqual(ss.count_visibility_vertical(2), 1)
-        self.assertEqual(ss.count_visibility_vertical(3), 2)
-        self.assertEqual(ss.count_visibility_vertical(4), 4)
+        self.assertEqual(ss.count_visibility_right(0), 3)
+        self.assertEqual(ss.count_visibility_right(1), 3)
+        self.assertEqual(ss.count_visibility_right(2), 3)
+        self.assertEqual(ss.count_visibility_right(3), 1)
+        self.assertEqual(ss.count_visibility_right(4), 2)
+
+        self.assertEqual(ss.count_visibility_up(0), 3)
+        self.assertEqual(ss.count_visibility_up(1), 2)
+        self.assertEqual(ss.count_visibility_up(2), 1)
+        self.assertEqual(ss.count_visibility_up(3), 2)
+        self.assertEqual(ss.count_visibility_up(4), 4)
+
+        self.assertEqual(ss.count_visibility_down(0), 3)
+        self.assertEqual(ss.count_visibility_down(1), 3)
+        self.assertEqual(ss.count_visibility_down(2), 3)
+        self.assertEqual(ss.count_visibility_down(3), 1)
+        self.assertEqual(ss.count_visibility_down(4), 2)
