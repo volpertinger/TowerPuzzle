b3c5f377103f27bbb06bab89a8bb6ebe70d07def
volpertinger <merzlovnik@mail.ru>
2021-11-12 17:12:37 +0300

    count_unfilled developed

--- TowerPuzzle.py
+++ TowerPuzzle.py
@@ -11,11 +11,12 @@ class TowerPuzzle:
         self.visibility_down = visibility_[3]
 
         self.field = [[0] * len(visibility_[0])] * len(visibility_[0])
+        self.size = len(visibility_[0])
 
     def count_visibility_left(self, index):
         result = 0
         max_height = 0
-        for i in range(len(self.field[0])):
+        for i in range(self.size):
             if max_height < self.field[index][i]:
                 max_height = self.field[index][i]
                 result += 1
@@ -24,17 +25,16 @@ class TowerPuzzle:
     def count_visibility_right(self, index):
         result = 0
         max_height = 0
-        length = len(self.field[0])
-        for i in range(length):
-            if max_height < self.field[index][length - i - 1]:
-                max_height = self.field[index][length - i - 1]
+        for i in range(self.size):
+            if max_height < self.field[index][self.size - i - 1]:
+                max_height = self.field[index][self.size - i - 1]
                 result += 1
         return result
 
     def count_visibility_up(self, index):
         result = 0
         max_height = 0
-        for i in range(len(self.field[0])):
+        for i in range(self.size):
             if max_height < self.field[i][index]:
                 max_height = self.field[i][index]
                 result += 1
@@ -43,10 +43,23 @@ class TowerPuzzle:
     def count_visibility_down(self, index):
         result = 0
         max_height = 0
-        length = len(self.field[0])
-        for i in range(length):
-            if max_height < self.field[length - i - 1][index]:
-                max_height = self.field[length - i - 1][index]
+        for i in range(self.size):
+            if max_height < self.field[self.size - i - 1][index]:
+                max_height = self.field[self.size - i - 1][index]
+                result += 1
+        return result
+
+    def count_unfilled_cells_horizontal(self, index):
+        result = 0
+        for i in range(self.size):
+            if self.field[index][i] == 0:
+                result += 1
+        return result
+
+    def count_unfilled_cells_vertical(self, index):
+        result = 0
+        for i in range(self.size):
+            if self.field[i][index] == 0:
                 result += 1
         return result
 
@@ -56,7 +69,7 @@ class TowerPuzzle:
         visibility_right_current = []
         visibility_down_current = []
 
-        for i in range(len(self.visibility_down)):
+        for i in range(self.size):
             visibility_left_current.append(self.count_visibility_left(i))
             visibility_right_current.append(self.count_visibility_right(i))
             visibility_up_current.append(self.count_visibility_up(i))
@@ -64,7 +77,7 @@ class TowerPuzzle:
         return (visibility_left_current == self.visibility_left) and (
                 visibility_right_current == self.visibility_right) and (
                        visibility_up_current == self.visibility_up) and (
-                           visibility_down_current == self.visibility_down)
+                       visibility_down_current == self.visibility_down)
 
 
 if __name__ == '__main__':
