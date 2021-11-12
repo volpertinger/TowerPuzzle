0c564ac144fb7d172ed067a624783918208a5b47
volpertinger <merzlovnik@mail.ru>
2021-11-12 15:31:41 +0300

    visibility_count rework

--- TowerPuzzle.py
+++ TowerPuzzle.py
@@ -9,9 +9,10 @@ class TowerPuzzle:
         self.visibility_up = visibility_[1]
         self.visibility_right = visibility_[2]
         self.visibility_down = visibility_[3]
+
         self.field = [[0] * len(visibility_[0])] * len(visibility_[0])
 
-    def count_visibility_horizontal(self, index):
+    def count_visibility_left(self, index):
         result = 0
         max_height = 0
         for i in range(len(self.field[0])):
@@ -20,7 +21,17 @@ class TowerPuzzle:
                 result += 1
         return result
 
-    def count_visibility_vertical(self, index):
+    def count_visibility_right(self, index):
+        result = 0
+        max_height = 0
+        length = len(self.field[0])
+        for i in range(length):
+            if max_height < self.field[index][length - i - 1]:
+                max_height = self.field[index][length - i - 1]
+                result += 1
+        return result
+
+    def count_visibility_up(self, index):
         result = 0
         max_height = 0
         for i in range(len(self.field[0])):
@@ -28,6 +39,32 @@ class TowerPuzzle:
                 max_height = self.field[i][index]
                 result += 1
         return result
+
+    def count_visibility_down(self, index):
+        result = 0
+        max_height = 0
+        length = len(self.field[0])
+        for i in range(length):
+            if max_height < self.field[length - i - 1][index]:
+                max_height = self.field[length - i - 1][index]
+                result += 1
+        return result
+
+    def is_solved(self):
+        visibility_left_current = []
+        visibility_up_current = []
+        visibility_right_current = []
+        visibility_down_current = []
+
+        for i in range(len(self.visibility_down)):
+            visibility_left_current.append(self.count_visibility_left(i))
+            visibility_right_current.append(self.count_visibility_right(i))
+            visibility_up_current.append(self.count_visibility_up(i))
+            visibility_down_current.append(self.count_visibility_down(i))
+        return (visibility_left_current == self.visibility_left) and (
+                visibility_right_current == self.visibility_right) and (
+                       visibility_up_current == self.visibility_up) and (
+                           visibility_down_current == self.visibility_down)
 
 
 if __name__ == '__main__':
