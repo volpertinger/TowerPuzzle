53de034b342dc71a2c82591c74ea7de652fece30
volpertinger <merzlovnik@mail.ru>
2021-11-13 11:44:35 +0300

    Cell class developed (for base field)

--- TowerPuzzle.py
+++ TowerPuzzle.py
@@ -2,6 +2,51 @@ import sys
 
 
 class TowerPuzzle:
+    class Cell:
+        def __init__(self, size):
+            # если в процессе работы программы в этом массиве будет 0, то это так вычеркнут невозможный вариант
+            self.array = []
+            for i in range(size):
+                self.array.append(i + 1)
+
+        def __str__(self):
+            result = '['
+            for i in range(len(self.array)):
+                if self.array[i] == 0:
+                    result += '-'
+                else:
+                    result += self.array[i]
+            result += ']'
+            return result
+
+        def __int__(self):
+            # если однозначно определили число в клетке
+            if self.array.count(0) == len(self.array) - 1:
+                for i in range(len(self.array)):
+                    if self.array[i] != 0:
+                        return self.array[i]
+            # если явно число еще не определено в клетке, то считаем, что в ней ничего не стоит (высота здания == 0)
+            else:
+                return 0
+
+        def set(self, number_):
+            if (number_ > len(self.array)) or (number_ < 1):
+                raise RuntimeError("Number " + str(number_) + " doesn't exist in cell")
+            result = [0] * len(self.array)
+            result[number_ - 1] = number_
+            self.array = result
+
+        def remove(self, number_):
+            if (number_ > len(self.array)) or (number_ < 1):
+                raise RuntimeError("Number " + str(number_) + " doesn't exist in cell")
+            self.array[number_ - 1] = 0
+
+        def get_not_zeros(self):
+            result = []
+            for i in range(len(self.array)):
+                if self.array[i] != 0:
+                    result.append(self.array[i])
+            return result
 
     def __init__(self, visibility_, field_=None):
         # visibility - числа по краям поля
@@ -16,53 +61,53 @@ class TowerPuzzle:
             self.field = field_
         self.size = len(visibility_[0])
 
-    def count_visibility_left(self, index):
+    def count_visibility_left(self, index_):
         result = 0
         max_height = 0
         for i in range(self.size):
-            if max_height < self.field[index][i]:
-                max_height = self.field[index][i]
+            if max_height < self.field[index_][i]:
+                max_height = self.field[index_][i]
                 result += 1
         return result
 
-    def count_visibility_right(self, index):
+    def count_visibility_right(self, index_):
         result = 0
         max_height = 0
         for i in range(self.size):
-            if max_height < self.field[index][self.size - i - 1]:
-                max_height = self.field[index][self.size - i - 1]
+            if max_height < self.field[index_][self.size - i - 1]:
+                max_height = self.field[index_][self.size - i - 1]
                 result += 1
         return result
 
-    def count_visibility_up(self, index):
+    def count_visibility_up(self, index_):
         result = 0
         max_height = 0
         for i in range(self.size):
-            if max_height < self.field[i][index]:
-                max_height = self.field[i][index]
+            if max_height < self.field[i][index_]:
+                max_height = self.field[i][index_]
                 result += 1
         return result
 
-    def count_visibility_down(self, index):
+    def count_visibility_down(self, index_):
         result = 0
         max_height = 0
         for i in range(self.size):
-            if max_height < self.field[self.size - i - 1][index]:
-                max_height = self.field[self.size - i - 1][index]
+            if max_height < self.field[self.size - i - 1][index_]:
+                max_height = self.field[self.size - i - 1][index_]
                 result += 1
         return result
 
-    def count_unfilled_cells_horizontal(self, index):
+    def count_unfilled_cells_horizontal(self, index_):
         result = 0
         for i in range(self.size):
-            if self.field[index][i] == 0:
+            if self.field[index_][i] == 0:
                 result += 1
         return result
 
-    def count_unfilled_cells_vertical(self, index):
+    def count_unfilled_cells_vertical(self, index_):
         result = 0
         for i in range(self.size):
-            if self.field[i][index] == 0:
+            if self.field[i][index_] == 0:
                 result += 1
         return result
 
