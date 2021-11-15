2cd96486256b657f48479a65df33a188e7b8adaa
volpertinger <merzlovnik@mail.ru>
2021-11-15 22:09:49 +0300

    solving with empty visibility test

--- Tests.py
+++ Tests.py
@@ -506,6 +506,24 @@ class Test_TowerPuzzle(unittest.TestCase):
         ss.solve()
         self.assertEqual(ss._TowerPuzzle__field, field)
 
+    def test_solve_with_empty_visibility(self):
+        visibility = [[1, 4, 0, 2], [1, 2, 3, 2], [4, 1, 0, 2], [2, 1, 2, 3]]
+        field = [[0, 0, 0, 0], [0, 2, 0, 0], [2, 1, 4, 3], [0, 0, 0, 0]]
+        field = TowerPuzzle.get_field_from_array(field)
+        ss = TowerPuzzle(visibility, deepcopy(field))
+        field = [[4, 3, 2, 1], [1, 2, 3, 4], [2, 1, 4, 3], [3, 4, 1, 2]]
+        field = TowerPuzzle.get_field_from_array(field)
+        ss.solve()
+        self.assertEqual(ss._TowerPuzzle__field, field)
+
+        visibility = [[1, 4, 0, 2], [1, 2, 3, 2], [4, 1, 0, 2], [2, 1, 2, 3]]
+        field = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+        field = TowerPuzzle.get_field_from_array(field)
+        ss = TowerPuzzle(visibility, deepcopy(field))
+        field = [[4, 3, 2, 1], [1, 2, 3, 4], [2, 1, 4, 3], [3, 4, 1, 2]]
+        field = TowerPuzzle.get_field_from_array(field)
+        ss.solve()
+        self.assertEqual(ss._TowerPuzzle__field, field)
 
 
 class Test_Cell(unittest.TestCase):
